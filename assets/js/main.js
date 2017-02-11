1e88365c4c4665bb3a1e4803cfc78932e32362cf
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-11 10:00:02 +0100

    Fine tuning the removal of a todo

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,9 @@ $('li').click(function(){
 //Removing a to-do
 $('span').click(function(e){
     //removing the parent element of the span
-    $(this).parent().fadeOut().remove();
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
     //disable event bubbling
     e.stopPropagation();
 });
