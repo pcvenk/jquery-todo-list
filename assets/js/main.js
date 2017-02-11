12571f43d4da816f342739089de57b430214c078
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-11 10:43:17 +0100

    Added functionality for creating new to-dos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,25 @@
 //Checking off a completed to-do
-$('li').click(function(){
+$('ul').on('click', 'li', function(){
    $(this).toggleClass('completed');
 });
 
 //Removing a to-do
-$('span').click(function(e){
+$('ul').on('click', 'span', function(e){
     //removing the parent element of the span
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     //disable event bubbling
     e.stopPropagation();
+});
+
+//Adding a new to-do
+$('input[type="text"').keypress(function(e) {
+    //if Enter is pressed
+    if(e.which === 13){
+        var input = $(this).val();
+        $(this).val('');
+        //append new to-do to the end of the ul
+        $('ul').append('<li><span>X</span> ' + input + '</li>');
+    }
 });
