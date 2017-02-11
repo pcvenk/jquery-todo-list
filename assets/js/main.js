9bc3eb24445cbd473aef171dd534f3318cc36d37
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-11 09:57:14 +0100

    Deleting a todo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
+//Checking off a completed to-do
 $('li').click(function(){
    $(this).toggleClass('completed');
+});
+
+//Removing a to-do
+$('span').click(function(e){
+    //removing the parent element of the span
+    $(this).parent().fadeOut().remove();
+    //disable event bubbling
+    e.stopPropagation();
 });
